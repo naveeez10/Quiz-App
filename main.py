c29e3d315d7e31315d85d09df5ab923663923a82
Naviz Khoja <116066_nk.beit20@ldceahm.gujgov.edu.in>
2022-03-29 14:08:52 +0530

    Added Final Touches

--- main.py
+++ main.py
@@ -16,6 +16,3 @@ quiz = QuizBrain(question_bank)
 quiz_interface = QuizInterface(quiz)
 # while quiz.still_has_questions():
 #     quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
